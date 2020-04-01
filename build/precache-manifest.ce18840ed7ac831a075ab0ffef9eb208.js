65b2cd90321b1a94bb9c3ca8fd935ec18cef7e3b
kmasalam <39186564+kmasalam@users.noreply.github.com>
2020-04-01 18:00:49 +0600

    nex tcommit

---
+++ build/precache-manifest.ce18840ed7ac831a075ab0ffef9eb208.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6812714e7e681dabe1eb8e833ac0ed63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dcfa65d56d0d50baec9",
+    "url": "/static/css/2.636c4284.chunk.css"
+  },
+  {
+    "revision": "c3588ae1cfb2b1d22d1e",
+    "url": "/static/css/main.91b9db15.chunk.css"
+  },
+  {
+    "revision": "3dcfa65d56d0d50baec9",
+    "url": "/static/js/2.5d574a42.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.5d574a42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3588ae1cfb2b1d22d1e",
+    "url": "/static/js/main.5513e428.chunk.js"
+  },
+  {
+    "revision": "e0eaaf9b69a66dcaf317",
+    "url": "/static/js/runtime-main.d3921c04.js"
+  },
+  {
+    "revision": "191c1db17d6a3fbd8c851a3097d95747",
+    "url": "/static/media/banner-bg-1-1.191c1db1.png"
+  },
+  {
+    "revision": "fab0757e1f3fe740c0489ce6190e27f0",
+    "url": "/static/media/banner-bg-1-2.fab0757e.jpg"
+  },
+  {
+    "revision": "ab01b146aaedb574a56b270f7e91d745",
+    "url": "/static/media/blog-bg-1-1.ab01b146.png"
+  },
+  {
+    "revision": "a54e504d8fd64efa278c439c7651fa6b",
+    "url": "/static/media/cta-bg-1-1.a54e504d.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont3e6e.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont3e6e.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont3e6e.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont3e6e.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont3e6e.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfontd41d.674f50d2.eot"
+  },
+  {
+    "revision": "34ff02a3b91a9438a037cc36930b6e8c",
+    "url": "/static/media/inner-banner-bg-1-1.34ff02a3.jpg"
+  },
+  {
+    "revision": "3650b7736c182b75817c663047b8cd5f",
+    "url": "/static/media/nonid-icon6dac.3650b773.woff"
+  },
+  {
+    "revision": "a4bd09683c788ca1d93c63ea529eb4f4",
+    "url": "/static/media/nonid-icon6dac.a4bd0968.ttf"
+  },
+  {
+    "revision": "c0cf3adbfc3d9e3227a52c3ff97e22cc",
+    "url": "/static/media/nonid-icon6dac.c0cf3adb.svg"
+  },
+  {
+    "revision": "c0d9ab5252f716c2626599f69ac9cd7b",
+    "url": "/static/media/nonid-icon6dac.c0d9ab52.eot"
+  },
+  {
+    "revision": "973f5d1fccf228df0dd76045b77f4b08",
+    "url": "/static/media/preloader.973f5d1f.gif"
+  },
+  {
+    "revision": "177f854dee39a7c5b580208626190656",
+    "url": "/static/media/testi-bg-1-1.177f854d.png"
+  }
+]);
